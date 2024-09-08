9a79ef3528f58254106e41ffc22403d740c20618
hazimah <hazimah@goodsane.com>
2024-09-08 22:25:48 +0800

    Added permission seeder

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -26,6 +26,42 @@ class PermissionSeeder extends Seeder
                 ['name' => 'Padam Kampus', 'category' => 'Pengurusan Kampus'],
                 ['name' => 'Lihat Kampus', 'category' => 'Pengurusan Kampus'],
             ],
+            'Pengurusan Makmal Komputer' => [
+                ['name' => 'Tambah Makmal Komputer', 'category' => 'Pengurusan Makmal Komputer'],
+                ['name' => 'Edit Makmal Komputer', 'category' => 'Pengurusan Makmal Komputer'],
+                ['name' => 'Padam Makmal Komputer', 'category' => 'Pengurusan Makmal Komputer'],
+                ['name' => 'Lihat Makmal Komputer', 'category' => 'Pengurusan Makmal Komputer'],
+            ],
+            'Pengurusan Perisian' => [
+                ['name' => 'Tambah Perisian', 'category' => 'Pengurusan Perisian'],
+                ['name' => 'Edit Perisian', 'category' => 'Pengurusan Perisian'],
+                ['name' => 'Padam Perisian', 'category' => 'Pengurusan Perisian'],
+                ['name' => 'Lihat Perisian', 'category' => 'Pengurusan Perisian'],
+            ],
+            'Pengurusan Senarai Semak Proses Kerja' => [
+                ['name' => 'Tambah Senarai Semak Proses Kerja', 'category' => 'Pengurusan Senarai Semak Proses Kerja'],
+                ['name' => 'Edit Senarai Semak Proses Kerja', 'category' => 'Pengurusan Senarai Semak Proses Kerja'],
+                ['name' => 'Padam Senarai Semak Proses Kerja', 'category' => 'Pengurusan Senarai Semak Proses Kerja'],
+                ['name' => 'Lihat Senarai Semak Proses Kerja', 'category' => 'Pengurusan Senarai Semak Proses Kerja'],
+            ],
+            'Pengurusan Senarai Semak Makmal' => [
+                ['name' => 'Tambah Senarai Semak Makmal', 'category' => 'Pengurusan Senarai Semak Makmal'],
+                ['name' => 'Edit Senarai Semak Makmal', 'category' => 'Pengurusan Senarai Semak Makmal'],
+                ['name' => 'Padam Senarai Semak Makmal', 'category' => 'Pengurusan Senarai Semak Makmal'],
+                ['name' => 'Lihat Senarai Semak Makmal', 'category' => 'Pengurusan Senarai Semak Makmal'],
+            ],
+            'Pengurusan Rekod Selenggara Makmal' => [
+                ['name' => 'Tambah Rekod Selenggara Makmal', 'category' => 'Pengurusan Rekod Selenggara Makmal'],
+                ['name' => 'Edit Rekod Selenggara Makmal', 'category' => 'Pengurusan Rekod Selenggara Makmal'],
+                ['name' => 'Padam Rekod Selenggara Makmal', 'category' => 'Pengurusan Rekod Selenggara Makmal'],
+                ['name' => 'Lihat Rekod Selenggara Makmal', 'category' => 'Pengurusan Rekod Selenggara Makmal'],
+            ],
+            'Pengurusan Rekod Selenggara Komputer' => [
+                ['name' => 'Tambah Rekod Selenggara Komputer', 'category' => 'Pengurusan Rekod Selenggara Komputer'],
+                ['name' => 'Edit Rekod Selenggara Komputer', 'category' => 'Pengurusan Rekod Selenggara Komputer'],
+                ['name' => 'Padam Rekod Selenggara Komputer', 'category' => 'Pengurusan Rekod Selenggara Komputer'],
+                ['name' => 'Lihat Rekod Selenggara Komputer', 'category' => 'Pengurusan Rekod Selenggara Komputer'],
+            ],
             'Pengurusan Jawatan' => [
                 ['name' => 'Tambah Jawatan', 'category' => 'Pengurusan Jawatan'],
                 ['name' => 'Edit Jawatan', 'category' => 'Pengurusan Jawatan'],
