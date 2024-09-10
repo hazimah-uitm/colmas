aac23d8da04da527db82edf47727cc0d039790ff
hazimah <hazimah@goodsane.com>
2024-09-10 15:22:47 +0800

    added permission

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -18,28 +18,30 @@
         </a>
     </li>
 
-    <li class="{{ Request::routeIs('activity-log') ? 'mm-active' : '' }}">
-        <a href="{{ route('activity-log') }}">
-            <div class="parent-icon"><i class='bx bx-history'></i></div>
-            <div class="menu-title">Log Aktiviti</div>
-        </a>
-    </li>
+    @role('Superadmin')
+        <li class="{{ Request::routeIs('activity-log') ? 'mm-active' : '' }}">
+            <a href="{{ route('activity-log') }}">
+                <div class="parent-icon"><i class='bx bx-history'></i></div>
+                <div class="menu-title">Log Aktiviti</div>
+            </a>
+        </li>
 
-    <li class="menu-label">Pengurusan Pengguna</li>
+        <li class="menu-label">Pengurusan Pengguna</li>
 
-    <li class="{{ Request::is('user*') && !Request::is('user-role*') ? 'mm-active' : '' }}">
-        <a href="{{ route('user') }}">
-            <div class="parent-icon"><i class='bx bx-user-circle'></i></div>
-            <div class="menu-title">Pengguna</div>
-        </a>
-    </li>
+        <li class="{{ Request::is('user*') && !Request::is('user-role*') ? 'mm-active' : '' }}">
+            <a href="{{ route('user') }}">
+                <div class="parent-icon"><i class='bx bx-user-circle'></i></div>
+                <div class="menu-title">Pengguna</div>
+            </a>
+        </li>
 
-    <li class="{{ Request::is('user-role*') ? 'mm-active' : '' }}">
-        <a href="{{ route('user-role') }}">
-            <div class="parent-icon"><i class='bx bx-shield'></i></div>
-            <div class="menu-title">Peranan Pengguna</div>
-        </a>
-    </li>
+        <li class="{{ Request::is('user-role*') ? 'mm-active' : '' }}">
+            <a href="{{ route('user-role') }}">
+                <div class="parent-icon"><i class='bx bx-shield'></i></div>
+                <div class="menu-title">Peranan Pengguna</div>
+            </a>
+        </li>
+    @endrole
 
     <li class="menu-label">Pengurusan Maklumat</li>
     <li class="{{ Request::is('lab-management*') ? 'mm-active' : '' }}">
@@ -58,61 +60,65 @@
     </li>
 
     <li class="menu-label">Tetapan</li>
-    <li class="{{ Request::is('campus*') ? 'mm-active' : '' }}">
-        <a class="has-arrow" href="#">
-            <div class="parent-icon"><i class='bx bx-location-plus'></i></div>
-            <div class="menu-title">Lokasi</div>
-        </a>
-        <ul>
-            <li class="{{ Request::is('campus*') ? 'mm-active' : '' }}">
-                <a href="{{ route('campus') }}"><i class="bx bx-right-arrow-alt"></i>Kampus</a>
-            </li>
-        </ul>
-    </li>
-    <li class="{{ Request::is('computer-lab*') ? 'mm-active' : '' }}">
-        <a href="{{ route('computer-lab') }}">
-            <div class="parent-icon"><i class='bx bx-desktop'></i>
-            </div>
-            <div class="menu-title">Makmal Komputer</div>
-        </a>
-    </li>
+    @hasanyrole('Superadmin|admin')
+        <li class="{{ Request::is('campus*') ? 'mm-active' : '' }}">
+            <a class="has-arrow" href="#">
+                <div class="parent-icon"><i class='bx bx-location-plus'></i></div>
+                <div class="menu-title">Lokasi</div>
+            </a>
+            <ul>
+                <li class="{{ Request::is('campus*') ? 'mm-active' : '' }}">
+                    <a href="{{ route('campus') }}"><i class="bx bx-right-arrow-alt"></i>Kampus</a>
+                </li>
+            </ul>
+        </li>
+        <li class="{{ Request::is('computer-lab*') ? 'mm-active' : '' }}">
+            <a href="{{ route('computer-lab') }}">
+                <div class="parent-icon"><i class='bx bx-desktop'></i>
+                </div>
+                <div class="menu-title">Makmal Komputer</div>
+            </a>
+        </li>
+    @endhasanyrole
 
-    <li class="{{ request()->routeIs('software*', 'work-checklist*', 'lab-checklist*') ? 'mm-active' : ''  }}">
+    <li class="{{ request()->routeIs('software*', 'work-checklist*', 'lab-checklist*') ? 'mm-active' : '' }}">
         <a class="has-arrow">
             <div class="parent-icon"><i class='bx bx-briefcase-alt'></i></div>
             <div class="menu-title">Proses Selenggara</div>
         </a>
         <ul>
-            <li class="{{ request()->routeIs('software*') ? 'mm-active' : ''  }}">
+            <li class="{{ request()->routeIs('software*') ? 'mm-active' : '' }}">
                 <a href="{{ route('software') }}"><i class="bx bx-right-arrow-alt"></i>Perisian</a>
             </li>
-            <li class="{{ request()->routeIs('work-checklist*') ? 'mm-active' : ''  }}">
+            <li class="{{ request()->routeIs('work-checklist*') ? 'mm-active' : '' }}">
                 <a href="{{ route('work-checklist') }}"><i class="bx bx-right-arrow-alt"></i>Proses Kerja</a>
             </li>
-            <li class="{{ request()->routeIs('lab-checklist*') ? 'mm-active' : ''  }}">
+            <li class="{{ request()->routeIs('lab-checklist*') ? 'mm-active' : '' }}">
                 <a href="{{ route('lab-checklist') }}"><i class="bx bx-right-arrow-alt"></i>Senarai Semak Makmal</a>
             </li>
         </ul>
     </li>
 
-
-    <li class="{{ Request::is('position*') ? 'mm-active' : '' }}">
-        <a class="has-arrow" href="#">
-            <div class="parent-icon"><i class="bx bx-cog"></i></div>
-            <div class="menu-title">Tetapan Umum</div>
-        </a>
-        <ul>
-            <li class="{{ Request::is('position*') ? 'mm-active' : '' }}">
-                <a href="{{ route('position') }}"><i class="bx bx-right-arrow-alt"></i>Jawatan</a>
-            </li>
-        </ul>
-    </li>
-
-    <li class="{{ Request::routeIs('logs.debug') ? 'mm-active' : '' }}">
-        <a href="{{ route('logs.debug') }}">
-            <div class="parent-icon"><i class='bx bxs-bug'></i></div>
-            <div class="menu-title">Debug Log</div>
-        </a>
-    </li>
+    @hasanyrole('Superadmin|admin')
+        <li class="{{ Request::is('position*') ? 'mm-active' : '' }}">
+            <a class="has-arrow" href="#">
+                <div class="parent-icon"><i class="bx bx-cog"></i></div>
+                <div class="menu-title">Tetapan Umum</div>
+            </a>
+            <ul>
+                <li class="{{ Request::is('position*') ? 'mm-active' : '' }}">
+                    <a href="{{ route('position') }}"><i class="bx bx-right-arrow-alt"></i>Jawatan</a>
+                </li>
+            </ul>
+        </li>
+    @endhasanyrole
+    @role('Superadmin')
+        <li class="{{ Request::routeIs('logs.debug') ? 'mm-active' : '' }}">
+            <a href="{{ route('logs.debug') }}">
+                <div class="parent-icon"><i class='bx bxs-bug'></i></div>
+                <div class="menu-title">Debug Log</div>
+            </a>
+        </li>
+    @endrole
 </ul>
 <!--end navigation-->
