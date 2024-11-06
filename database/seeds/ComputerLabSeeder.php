864765bfcbab69f8ea2f9c74c2044964c619f134
hazimah <hazimahpte@gmail.com>
2024-11-06 08:38:05 +0800

    Updated seeder

--- database/seeds/ComputerLabSeeder.php
+++ database/seeds/ComputerLabSeeder.php
@@ -17,12 +17,32 @@ class ComputerLabSeeder extends Seeder
                 'code' => 'B4001',
                 'name' => 'Makmal Komputer 1',
                 'campus_id' => 1,
-                'pemilik_id' => 2,
+                'pemilik_id' => 3,
                 'username' => 'UiTM',
+                'password' => 'uitm123',
+                'no_of_computer' => 5,
+                'publish_status' => true
+            ],
+            [
+                'code' => 'B4002',
+                'name' => 'Makmal Komputer 2',
+                'campus_id' => 2,
+                'pemilik_id' => 2,
+                'username' => 'UiTM 1',
+                'password' => 'uitm123',
+                'no_of_computer' => 3,
+                'publish_status' => true
+            ],
+            [
+                'code' => 'B4003',
+                'name' => 'Makmal Komputer 3',
+                'campus_id' => 2,
+                'pemilik_id' => 2,
+                'username' => 'UiTM 1',
                 'password' => 'uitm123',
                 'no_of_computer' => 2,
                 'publish_status' => true
-            ]
+            ],
         ]);
     }
 }
